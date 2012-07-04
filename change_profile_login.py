1acbfa07f1bb304f0f95e78d0c2c9e0632a0e7d7
melch8805a <melch8805a@gmail.com>
2012-07-03 23:21:41 -1000

    Change vars and tested output

--- change_profile_login.py
+++ change_profile_login.py
@@ -7,22 +7,24 @@ droid.dialogCreateAlert("Profile Switcher","Would you like to switch now?")
 droid.dialogSetPositiveButtonText("Yes")
 droid.dialogSetNegativeButtonText("No")
 droid.dialogShow()
-response = droid.dialogGetResponse().result
+switch = droid.dialogGetResponse().result
 droid.dialogDismiss()
 
 #Response handling for dialog box
-if response.has_key("which"):
-	result = response["which"]
+if switch.has_key("which"):
+	result = switch["which"]
 	if result == "positive":
-		droid.dialogGetInput("User Name", "Please Enter Your User Name:")
-		droid.dialogGetPassword("Password", "Please Enter Your Password:")
+		username = droid.dialogGetInput("User Name", "Please Enter Your User Name:").result
+		password = droid.dialogGetPassword("Password", "Please Enter Your Password:").result
 	elif result == "negative":
 		droid.makeToast("Thank you, come again!")
-elif response.has_key("canceled"):
+elif switch.has_key("canceled"):
 	droid.makeToast("Oops, that was the back button...")
 
 # Random error handler....
 else:
 	print "Unknown response=",response
 
-print "Done"
+# this is here just to make sure username / password was saving off correctly
+droid.makeToast(username)
+droid.makeToast(password)
